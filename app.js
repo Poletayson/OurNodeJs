f8c4cf7026a636fda1b2a5bb2124b3dd5ab6b882
Poletayson <dmitriipoletaev_1997@mail.ru>
2019-04-24 02:12:30 +0700

    Пахнет рабочим редактированием через пост

--- app.js
+++ app.js
@@ -63,18 +63,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/public', express.static('public'));
+
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/products/:idd', oneProductsRouter);
-
+app.use('/products/:idd/save', oneProductsRouter);
 
 
 
--- routes/oneProduct.js
+++ routes/oneProduct.js
@@ -4,39 +4,51 @@ var router = express.Router({ mergeParams: true });
 var exp = require("../app.js");
 
 
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-//
-//
-//
-//
-// // установка схемы
-// const productSch = new Schema({
-//     title: String,
-//     code: String,
-//     description: String,
-//     img: String,
-//     balance: Number
-// });
-//
-// // подключение
-// mongoose.connect("mongodb://localhost/ourdb", { useNewUrlParser: true });
 const Product = exp.Product;
 
-
-
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('', function(req, res, next) {
 
     Product.findOne({ _id: req.params.idd }, function(err, prod) {
-        //console.log(req.query);
-        console.log("Продукт ",prod);
+        //console.log("Длинища ", Object.keys(req.query).length);
+        if (Object.keys(req.query).length > 0)
+        {
+            console.log("Параметры ", req.query);
+            var img = req.query.img;
+            var title = req.query.title;
+            var code = req.query.cod;
+            var balance = req.query.balance;
+            var description = req.query.description;
+//var title = req.body.title;
+            console.log("fdhdfhfhf ",title);
+            // router.get('/', function(req, res, next) {
 
-        res.render('productPage', {
-            product: prod                     //include(\"/index.ejs\")
-        });
+            Product.findOne({ _id: req.params.idd }, function(err, prod) {
+                 console.log("Функция сохранения для ",prod);
+                prod.img = img;
+                prod.title = title;
+                prod.code = code;
+                prod.balance = balance;
+                prod.description = description;
+
+                prod.save(function(err){
+                    //mongoose.disconnect();  // отключение от базы данных
+
+                    if(err) return console.log(err);
+                    console.log("Сохранен объект", prod);
+                });
+
+                //req.query.
+                //document.location.href ("http://127.0.0.1:3000/products")
+                //return res.redirect("http://127.0.0.1:3000/products");
+            });
+        }
+
+            res.render('productPage', {
+                product: prod                     //include(\"/index.ejs\")
+            });
+
+
 
     });
 
@@ -45,22 +57,26 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.post('/save', function(req, res, next) {
-    console.log("Зашли сюда");
+    //console.log("Зашли сюда");
 
     //var exp = require("../../app.js");
-    var img = document.getElementById("img").value;
-    var title = document.getElementById("title").value;
-    var code = document.getElementById("code").value;
-    var balance = document.getElementById("balance").value;
-    var description = document.getElementById("description").value;
-
-
+    var img = req.body.img;
+    var title = req.body.title;
+    var code = req.body.code;
+    var balance = req.body.balance;
+    var description = req.body.description;
+//var title = req.body.title;
+    //console.log("fdhdfhfhf ",title);
     // router.get('/', function(req, res, next) {
 
     Product.findOne({ _id: req.params.idd }, function(err, prod) {
         //console.log(req.query);
         console.log("Функция сохранения для ",prod);
         prod.img = img;
+        prod.title = title;
+        prod.code = code;
+        prod.balance = balance;
+        prod.description = description;
 
         prod.save(function(err){
             //mongoose.disconnect();  // отключение от базы данных
@@ -68,30 +84,22 @@ router.post('/save', function(req, res, next) {
             if(err) return console.log(err);
             console.log("Сохранен объект", prod);
         });
-
-
-        res.redirect('');
-
-
-
-
-
+        //window.history.back();
+        var str = '/products/' + prod._id;
+        res.redirect(str);
     });
 
 
 
-
-
-
-    Product.findOne({ _id: req.params.idd }, function(err, prod) {
-        //console.log(req.query);
-        console.log("Продукт ",prod);
-
-        res.render('productPage', {
-            product: prod                     //include(\"/index.ejs\")
-        });
-
-    });
+    // Product.findOne({ _id: req.params.idd }, function(err, prod) {
+    //     //console.log(req.query);
+    //     console.log("Продукт ",prod);
+    //
+    //     res.render('productPage', {
+    //         product: prod                     //include(\"/index.ejs\")
+    //     });
+    //
+    // });
 
 });
 

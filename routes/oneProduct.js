132306c7d4dfa374bc907ef83574df3611255d6a
Poletayson <dmitriipoletaev_1997@mail.ru>
2019-04-23 23:18:16 +0700

    Гора фигни

--- routes/oneProduct.js
+++ routes/oneProduct.js
@@ -40,6 +40,58 @@ router.get('/', function(req, res, next) {
 
     });
 
+});
+
+
+/* GET home page. */
+router.post('/save', function(req, res, next) {
+    console.log("Зашли сюда");
+
+    //var exp = require("../../app.js");
+    var img = document.getElementById("img").value;
+    var title = document.getElementById("title").value;
+    var code = document.getElementById("code").value;
+    var balance = document.getElementById("balance").value;
+    var description = document.getElementById("description").value;
+
+
+    // router.get('/', function(req, res, next) {
+
+    Product.findOne({ _id: req.params.idd }, function(err, prod) {
+        //console.log(req.query);
+        console.log("Функция сохранения для ",prod);
+        prod.img = img;
+
+        prod.save(function(err){
+            //mongoose.disconnect();  // отключение от базы данных
+
+            if(err) return console.log(err);
+            console.log("Сохранен объект", prod);
+        });
+
+
+        res.redirect('');
+
+
+
+
+
+    });
+
+
+
+
+
+
+    Product.findOne({ _id: req.params.idd }, function(err, prod) {
+        //console.log(req.query);
+        console.log("Продукт ",prod);
+
+        res.render('productPage', {
+            product: prod                     //include(\"/index.ejs\")
+        });
+
+    });
 
 });
 
